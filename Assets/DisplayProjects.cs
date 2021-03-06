f5a53d883f35ffe75a113e05f900272de918e107
ATTALA Kheireddine <gk_attala@esi.dz>
2021-03-06 10:35:03 +0100

    code works screen works, needs a back to main menu button and images for the scriptables

---
+++ Assets/DisplayProjects.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayProjects : MonoBehaviour
+{
+    [SerializeField] RectTransform contentRect;
+    [SerializeField] GameObject projectPrefabImgRight, projectPrefabImgLeft;
+    [SerializeField] ProjectData[] allProjectDatas;
+    [SerializeField] Scrollbar scrollbar;
+
+    float projectImageRightHeight, projectImageLeftHeight;
+    void Start()
+    {
+        projectImageLeftHeight = projectPrefabImgLeft.GetComponent<RectTransform>().rect.height;
+        projectImageRightHeight = projectPrefabImgRight.GetComponent<RectTransform>().rect.height;
+        DisplayAllProjects();
+    }
+    
+    void displayProjects(ProjectData[] projects)
+    {
+        // we assume we start from a "right" one, we could have picked the opposite case but anyway
+        foreach (Transform t in contentRect.transform)
+        {
+            Destroy(t.gameObject);
+        }
+        float startHeight = projectImageRightHeight / 2;
+        float currHeight = startHeight;
+        
+        bool indexIsPair = false; //will help us know the pair and impair indexes in foreach
+        foreach (ProjectData pData in projects)
+        {
+            if (indexIsPair)
+            {
+                RectTransform instance = Instantiate(projectPrefabImgLeft, contentRect).GetComponent<RectTransform>();
+                Vector3 position = instance.localPosition;
+                instance.localPosition = new Vector3(position.x, -currHeight, position.z);
+                instance.GetComponent<DisplayProject>().DisplayProjectFromData(pData);
+
+            }
+
+            else
+            {
+                RectTransform instance = Instantiate(projectPrefabImgRight, contentRect).GetComponent<RectTransform>();
+                Vector3 position = instance.localPosition;
+                instance.localPosition = new Vector3(position.x, -currHeight, position.z);
+                instance.GetComponent<DisplayProject>().DisplayProjectFromData(pData);
+            }
+            
+            currHeight += projectImageRightHeight;
+            indexIsPair = !indexIsPair;
+        }
+        Rect rect = contentRect.rect;
+        scrollbar.value = 1; //reset the scrollBar to the top
+        contentRect.sizeDelta = new Vector2(0, currHeight);
+    }
+
+    public void DisplayAllProjects()
+    {
+        displayProjects(allProjectDatas);
+    }
+
+    public void DisplayProjectsWithTag(ProjectTag tag)
+    {
+        List<ProjectData> filteredList = new List<ProjectData>();
+        foreach(ProjectData pd in allProjectDatas)
+        {
+            foreach(ProjectTag t in pd.tags)
+            {
+                if (t==tag)
+                {
+                    filteredList.Add(pd);
+                }
+            }
+        }
+        displayProjects(filteredList.ToArray());
+    }
+}
---
+++ Assets/DisplayProjectsOnTagButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayProjectsOnTagButton : MonoBehaviour
+{
+    [SerializeField] ProjectTag projectTag;
+    [SerializeField] DisplayProjects displayManager;
+    // Start is called before the first frame update
+    public void OnClick()
+    {
+        displayManager.DisplayProjectsWithTag(projectTag);
+    }
+}
